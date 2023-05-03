505cdc88f357c397a4cc5c0d9137021996d24b5c
A.Polyakov <a.polyakov@unlimint.com>
2023-05-03 08:49:44 +0300

    groovy: syntax

--- groovy-test/src/main/groovy/org/example/Main.groovy
+++ groovy-test/src/main/groovy/org/example/Main.groovy
@@ -9,8 +9,18 @@ class RomanNumber {
         this.number = number
     }
 
+    RomanNumber next() {
+        Integer res = numbers.indexOf(number.toUpperCase()) + 1
+        if (res >= numbers.size()) {
+            throw new UnsupportedOperationException("Sorry, number is too big")
+        }
+        number = numbers[res]
+        this
+    }
+
     RomanNumber plus(RomanNumber other) {
         Integer res = numbers.indexOf(number.toUpperCase()) + numbers.indexOf(other.number.toUpperCase()) + 1
+        assert res < numbers.size()
         if (res >= numbers.size()) {
             throw new UnsupportedOperationException("Sorry, number is too big")
         }
@@ -33,7 +43,10 @@ class RomanNumber {
 static void main(String[] args) {
     println "Hello world!"
     def one = new RomanNumber('i')
-    def two = new RomanNumber('II')
+    def two = new RomanNumber('V')
     def three = one + two
     println three
+
+    one++
+    println one
 }
